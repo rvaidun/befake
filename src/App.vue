00903fa1254bedd86112c64a4ac65d2eb9337926
rvaidun <19789246+rvaidun@users.noreply.github.com>
2022-07-10 11:48:33 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex items-center justify-center mt-[25%]">
+    <span class="mr-2">Enter your phone number with country code and +: </span>
+    <input
+      type="text"
+      placeholder="+11234567890"
+      v-model="phone"
+      class="border-black border-2"
+    />
+  </div>
+</template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      phone: "",
+    };
+  },
+};
+</script>
