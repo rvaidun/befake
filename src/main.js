5cdc88e24670f2180f492198bab152154a531f9f
rvaidun <19789246+rvaidun@users.noreply.github.com>
2022-07-15 14:15:25 -0700

    done

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp, VueElement } from "vue";
 import App from "./App.vue";
 import "./index.css";
+import VueClipboard from "vue3-clipboard";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(VueClipboard, {
+  autoSetContainer: true,
+  appendToBody: true,
+});
+app.mount("#app");
