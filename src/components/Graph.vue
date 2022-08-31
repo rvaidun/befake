81f164073bcac6709d64b4c4460cc578435d5696
rvaidun <19789246+rvaidun@users.noreply.github.com>
2022-08-31 15:45:47 -0700

    adding routes

---
+++ src/components/Graph.vue
@@ -0,0 +1,15 @@
+<script>
+import * as d3 from "d3";
+export default {
+  mounted() {
+    const width = 960;
+    const height = 500;
+    const svg = d3.select("svg").attr("width", width).attr("height", height);
+    const g = svg.append("g");
+  },
+};
+</script>
+<template>
+  <span class="text-9xl text-white">Hello</span>
+  <svg></svg>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<script>
+import loginComponent from "./loginComponent.vue";
+import postsComponentVue from "./postsComponent.vue";
+
+export default {
+  components: {
+    loginComponent,
+    postsComponentVue,
+  },
+  data() {
+    return {
+      loggedIn: localStorage.getItem("token") ? true : false,
+    };
+  },
+  methods: {
+    login() {
+      this.loggedIn = true;
+    },
+    logout() {
+      localStorage.clear();
+      this.loggedIn = false;
+    },
+  },
+};
+</script>
+
+<template>
+  <loginComponent v-if="!loggedIn" :login="login" />
+  <postsComponentVue v-else :logout="logout" />
+</template>
