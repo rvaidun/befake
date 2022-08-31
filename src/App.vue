f49fb5a070f8678c1c0a5f60c91bffdaa7cdbd15
rvaidun <19789246+rvaidun@users.noreply.github.com>
2022-08-31 15:45:47 -0700

    adding routes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,3 @@
-<script>
-import loginComponent from "./components/loginComponent.vue";
-import postsComponentVue from "./components/postsComponent.vue";
-import { defineComponent } from "vue";
-export default defineComponent({
-  components: {
-    loginComponent,
-    postsComponentVue,
-  },
-  data() {
-    return {
-      loggedIn: localStorage.getItem("token") ? true : false,
-    };
-  },
-  methods: {
-    login() {
-      this.loggedIn = true;
-    },
-    logout() {
-      localStorage.clear();
-      this.loggedIn = false;
-    },
-  },
-});
-</script>
-
 <template>
-  <loginComponent v-if="!loggedIn" :login="login" />
-  <postsComponentVue v-else :logout="logout" />
+  <router-view />
 </template>
