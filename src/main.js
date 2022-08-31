d3e9d9ea2788976065d0967b0a77690c640d3877
rvaidun <19789246+rvaidun@users.noreply.github.com>
2022-08-31 15:45:47 -0700

    adding routes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 import { createApp, VueElement } from "vue";
-import App from "./App.vue";
+import Home from "./components/Home.vue";
+import Graph from "./components/Graph.vue";
 import "./index.css";
+import App from "./App.vue";
 import VueClipboard from "vue3-clipboard";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from "vue-router";
+const routes = [
+  { path: "/", component: Home },
+  { path: "/graph", component: Graph },
+];
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHistory(),
+  routes, // short for `routes: routes`
+});
 
 const app = createApp(App);
+app.use(router);
+
 app.use(VueClipboard, {
   autoSetContainer: true,
   appendToBody: true,
