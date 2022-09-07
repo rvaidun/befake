9eebe8e788f2fb96da4d5f8718b94f2c3e5051da
rvaidun <19789246+rvaidun@users.noreply.github.com>
2022-09-07 06:27:22 +0530

    added tracking

--- src/main.js
+++ src/main.js
@@ -4,11 +4,8 @@ import Graph from "./components/Graph.vue";
 import "./index.css";
 import App from "./App.vue";
 import VueClipboard from "vue3-clipboard";
-import {
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
+import VueGtag from "vue-gtag";
 const routes = [
   { path: "/", component: Home },
   { path: "/graph", component: Graph },
@@ -21,7 +18,13 @@ const router = createRouter({
 
 const app = createApp(App);
 app.use(router);
-
+app.use(
+  VueGtag,
+  {
+    config: { id: "G-ZJ1LQLNXRG" },
+  },
+  router
+);
 app.use(VueClipboard, {
   autoSetContainer: true,
   appendToBody: true,
