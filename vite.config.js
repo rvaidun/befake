3e0db1ce60004885c8fc89fa9fb2921742ca8e09
rvaidun <19789246+rvaidun@users.noreply.github.com>
2022-09-07 06:27:22 +0530

    added tracking

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
