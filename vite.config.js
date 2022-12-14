d738f4598730b940e6425f89ca5345e2d777f261
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2022-12-14 00:42:10 -0800

    removed pwa

--- vite.config.js
+++ vite.config.js
@@ -3,32 +3,10 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "BeReal Viewer",
-        short_name: "BeReal Viewer",
-        description: "BeReal Viewer",
-        start_url: ".",
-        display: "standalone",
-        background_color: "#ffffff",
-        icons: [
-          {
-            src: "favicon.ico",
-            sizes: "64x64 32x32 24x24 16x16",
-            type: "image/x-icon",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
