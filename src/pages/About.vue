9b2d1e94c904d1aa0f36e3e8a899ae13cec61311
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2023-01-01 06:11:08 -0800

    happy new year

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,51 +1,117 @@
 <template>
-  <div
-    class="text-left text-white sm:flex flex-col justify-evenly space-x-10 w-full py-9 items-center"
-  >
+  <div class="flex justify-between mx-5">
+    <div class="flex-col items-center justify-center">
+      <div></div>
+      <div class="flex flex-col max-w-2xl space-y-3 items-center">
+        <img
+          src="https://raw.githubusercontent.com/rvaidun/rvaidun.github.io/main/images/myphoto.jpeg"
+          alt="Photo of Rahul Vaidun"
+          class="rounded-full w-48" />
+        <h1 class="text-5xl font-bold">Hi! 👋</h1>
+        <p class="text-base">
+          This website was created by
+          <span class="font-bold">Rahul Vaidun</span> I am a 20 year old college
+          student from California majoring in Computer Science.
+        </p>
+
+        <p class="text-base">
+          I created this website as a side project to learn more about Vue.js
+          and web development. I also wanted to reverse engineer BeReal's API
+          and create a web app that would allow users to view their BeReal data.
+          This happens to allow users to also view their friends BeReals without
+          posting their own BeReals and screenshot BeReals. This website was
+          designed from ground up to work on mobile devices. All the styling is
+          done with <span class="font-bold">Tailwind CSS</span>
+        </p>
+        <p class="text-base">
+          I don't have plans on monetizing this site hence there will
+          <span class="font-bold">never</span> be ads on this website at least
+          for the forseeable future. However I will happily accept donations.
+          Donation link can be seen in the navbar. If you are interested in
+          viewing the source code or making your own contributions to BeFake
+          check out our
+          <a class="text-blue-600" href="https://github.com/rvaidun/befake"
+            >github repository</a
+          >
+        </p>
+        <div class="flex flex-col items-center text-white">
+          <h1 class="text-3xl font-bold">Contact me</h1>
+          <div class="flex flex-col items-center space-y-1 text-blue-600">
+            <a href="https://www.twitter.com/rahulvaidun/">Twitter </a>
+            <a href="https://www.linkedin.com/in/rahulvaidun/">LinkedIn </a>
+            <a href="https://www.github.com/rvaidun/">Github </a>
+            <a href="https://www.instagram.com/rahulvaidun/">Instagram </a>
+            <!-- email -->
+            <a href="mailto:rahul.vaidun@gmail.com">Email</a>
+          </div>
+        </div>
+      </div>
+    </div>
     <div>
+      <blockquote class="twitter-tweet" data-theme="dark">
+        <p lang="en" dir="ltr">
+          <a
+            href="https://twitter.com/hashtag/HappyNewYear2023?src=hash&amp;ref_src=twsrc%5Etfw"
+            >#HappyNewYear2023</a
+          >
+          to all 12.7k active users on
+          <a href="https://t.co/cAj1Svil6t">https://t.co/cAj1Svil6t</a> this
+          year! 🥂🎉<br /><br />never imagined this site getting so much
+          traction and am very grateful for those who spread the word 🫶🏽<br /><br />looking
+          forward to 100k active users in 2023 provided
+          <a href="https://twitter.com/BeReal_App?ref_src=twsrc%5Etfw"
+            >@BeReal_App</a
+          >
+          does not patch it.
+          <a href="https://t.co/Q9cBD96FgH">pic.twitter.com/Q9cBD96FgH</a>
+        </p>
+        &mdash; rahul (@rahulvaidun)
+        <a
+          href="https://twitter.com/rahulvaidun/status/1609527994711965702?ref_src=twsrc%5Etfw"
+          >January 1, 2023</a
+        >
+      </blockquote>
+    </div>
+  </div>
+  <!-- <div
+    class="text-left text-white sm:flex-row flex-col justify-evenly space-x-10 w-full py-9 items-center">
+    <div class="flex-col flex items-center">
       <img
         src="https://raw.githubusercontent.com/rvaidun/rvaidun.github.io/main/images/myphoto.jpeg"
         alt="Photo of Rahul Vaidun"
-        class="rounded-full w-48"
-      />
-    </div>
-    <div class="flex flex-col max-w-2xl space-y-3">
-      <h1 class="text-5xl font-bold">Hi! 👋</h1>
-      <p class="text-base">
-        This website was created by
-        <span class="font-bold">Rahul Vaidun</span> I am a 20 year old college
-        student from California majoring in Computer Science.
-      </p>
+        class="rounded-full w-48" />
+      <div class="flex flex-col max-w-2xl space-y-3 items-center">
+        <h1 class="text-5xl font-bold">Hi! 👋</h1>
+        <p class="text-base">
+          This website was created by
+          <span class="font-bold">Rahul Vaidun</span> I am a 20 year old college
+          student from California majoring in Computer Science.
+        </p>
 
-      <p class="text-base">
-        I created this website as a side project to learn more about Vue.js and
-        web development. I also wanted to reverse engineer BeReal's API and
-        create a web app that would allow users to view their BeReal data. This
-        happens to allow users to also view their friends BeReals without
-        posting their own BeReals and screenshot BeReals. This website was
-        designed from ground up to work on mobile devices. All the styling is
-        done with <span class="font-bold">Tailwind CSS</span>
-      </p>
-      <p class="text-base">
-        I don't have plans on monetizing this site hence there will
-        <span class="font-bold">never</span> be ads on this website at least for
-        the forseeable future. If you are interested in viewing the source code
-        or making your own contributions to BeFake check out our
-        <a class="text-blue-600" href="https://github.com/rvaidun/befake"
-          >github repository</a
-        >
-      </p>
+        <p class="text-base">
+          I created this website as a side project to learn more about Vue.js
+          and web development. I also wanted to reverse engineer BeReal's API
+          and create a web app that would allow users to view their BeReal data.
+          This happens to allow users to also view their friends BeReals without
+          posting their own BeReals and screenshot BeReals. This website was
+          designed from ground up to work on mobile devices. All the styling is
+          done with <span class="font-bold">Tailwind CSS</span>
+        </p>
+        <p class="text-base">
+          I don't have plans on monetizing this site hence there will
+          <span class="font-bold">never</span> be ads on this website at least
+          for the forseeable future. However I will happily accept donations.
+          Donation link can be seen in the navbar. If you are interested in
+          viewing the source code or making your own contributions to BeFake
+          check out our
+          <a class="text-blue-600" href="https://github.com/rvaidun/befake"
+            >github repository</a
+          >
+        </p>
+      </div>
     </div>
-  </div>
-  <div class="flex flex-col items-center text-white py-8">
-    <h1 class="text-3xl font-bold">Contact me</h1>
-    <div class="flex flex-col items-center space-y-1 text-blue-600">
-      <a href="https://www.twitter.com/rahulvaidun/">Twitter </a>
-      <a href="https://www.linkedin.com/in/rahulvaidun/">LinkedIn </a>
-      <a href="https://www.github.com/rvaidun/">Github </a>
-      <a href="https://www.instagram.com/rahulvaidun/">Instagram </a>
-      <!-- email -->
-      <a href="mailto:rahul.vaidun@gmail.com">Email</a>
+    <div>
+      
     </div>
-  </div>
+  </div> -->
 </template>
