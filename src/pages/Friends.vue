b71c59b79b334fe1c7dafae36519a0d5eaf61fe3
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2023-01-07 23:51:03 -0800

    search

--- src/pages/Friends.vue
+++ src/pages/Friends.vue
@@ -6,11 +6,36 @@ export default {
   data() {
     return {
       username: "",
+      profiles: [],
+      nonefound: false,
     };
   },
   methods: {
     search() {
-      console.log("Here");
+      fetch(
+        `${this.$store.state.proxyUrl}/https://mobile.bereal.com/api/search/profile?query=${this.username}&limit=20`,
+        {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("token")}`,
+            // accept: "*/*",
+            // "bereal-platform": "iOS",
+            // "bereal-os-version": "14.7.1",
+            // "accept-language": "en-US;q=1.0",
+            // "bereal-device-id": "BE707B57-FDF8-4764-87FB-96367BB32018",
+            // "bereal-app-language": "en-US",
+            // "user-agent":
+            //   "BeReal/0.28.2 (AlexisBarreyat.BeReal; build:8425; iOS 14.7.1) 1.0.0/BRApiKit",
+            // "bereal-timezone": "America/Los_Angeles",
+            // "bereal-device-language": "en",
+            // "bereal-app-version": "0.28.2-(8425)",
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          this.profiles = res.data;
+          this.nonefound = res.data.length === 0;
+        });
     },
   },
 };
@@ -20,10 +45,30 @@ export default {
     <div class="flex items-center">
       <MyInput
         v-model="username"
-        placeholder="Comment"
+        placeholder="Username"
         class="w-96"
         @enter="search" />
       <MyButton @click="search">Search</MyButton>
+    </div>
+    <div class="flex flex-col items-center">
+      <div v-if="nonefound">
+        <div class="text-2xl text-red-800">No users found</div>
+      </div>
+      <div v-for="i in profiles" :key="i.id">
+        <div class="flex items-center gap-3">
+          <img
+            :src="
+              i.profilePicture
+                ? i.profilePicture.url
+                : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQotrbW-vxBRCZIOODdVJZ5NTaTjOnFveV500Dve9g&s'
+            "
+            class="w-24 h-24 rounded-[50%]" />
+          <div class="flex flex-col">
+            <div class="text-2xl">{{ i.username }}</div>
+            <div class="text-xl">{{ i.mutualFriends }} mutual friends</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
