5c60fc06aaa0ed3da6768431726c1702962e1a73
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2023-02-08 13:40:13 -0800

    post uploading works

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ const store = createStore({
     return {
       proxyUrl: "https://warm-scrubland-06418.herokuapp.com",
       loginUrl: "https://us-central1-befake-623af.cloudfunctions.net/login",
+      // loginUrl: "http://127.0.0.1:5001/befake-623af/us-central1/login",
       // proxyUrl: "http://localhost:8080",
       loggedIn: localStorage.getItem("token") ? true : false,
       posts: [],
