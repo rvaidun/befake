442819a10c77d2b0a088388086f3d1ffe6bfaf8f
xKRISTOFx <gral1322@gmail.com>
2023-04-12 03:07:14 +0200

    some minor fixes

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -37,56 +37,33 @@ export default {
       <div class="flex justify-between flex-col sm:flex-row">
         <div>
           <!-- logo -->
-          <a
-            class="flex items-center cursor-pointer"
-            v-clipboard:copy="copy"
-            href="/">
-            <img
-              src="../../assets/favicon.svg"
-              class="w-12 rounded-full object-cover mr-2" 
-              alt="logo"/>
+          <a class="flex items-center cursor-pointer" v-clipboard:copy="copy" href="/">
+            <img src="../../assets/favicon.svg" class="w-12 rounded-full object-cover mr-2" alt="logo" />
             <span class="text-3xl font-bold">BeFake</span>
           </a>
         </div>
-        <a
-          href="#"
-          @click="hideNavbar = !hideNavbar"
+        <a href="#" @click="hideNavbar = !hideNavbar"
           class="absolute top-[0.75rem] right-[1rem] sm:hidden flex flex-col justify-between w-[30px] h-[21px]">
           <span class="h-[3px] w-[100%] bg-white rounded-lg"></span>
           <span class="h-[3px] w-[100%] bg-white rounded-lg"></span>
           <span class="h-[3px] w-[100%] bg-white rounded-lg"></span>
         </a>
         <!-- links -->
-        <div
-          class="items-end space-x-2 flex flex-col sm:flex sm:flex-row p-[.5rem]"
-          :class="{
-            hidden: hideNavbar,
-          }">
-          <!-- <a
-            href="https://www.twitter.com/rahulvaidun/"
-            class="cursor-pointer text-blue-400 sm:py-2 py-[0.6px] sm:px-3 rounded-md font-bold">
-            Twitter
-          </a> -->
-
-          <a
-            class="cursor-pointer text-red-500 sm:py-2 py-[0.6px] sm:px-3 rounded-md font-bold"
+        <div class="items-end space-x-2 flex flex-col sm:flex sm:flex-row p-[.5rem]" :class="{
+          hidden: hideNavbar,
+        }">
+          <a class="cursor-pointer text-red-500 sm:py-2 py-[0.6px] sm:px-3 rounded-md font-bold"
             href="https://ko-fi.com/rahulvaidun">
             Donate
           </a>
-          <a
-            class="cursor-pointer text-white sm:py-2 py-[0.6px] sm:px-3 rounded-md font-bold"
-            href="/map">
+          <a class="cursor-pointer text-white sm:py-2 py-[0.6px] sm:px-3 rounded-md font-bold" href="/map">
             Map
           </a>
-          <a
-            class="cursor-pointer text-white sm:py-2 py-[0.6px] sm:px-3 rounded-md font-bold"
-            href="/about">
+          <a class="cursor-pointer text-white sm:py-2 py-[0.6px] sm:px-3 rounded-md font-bold" href="/about">
             About
           </a>
-          <a
-            class="cursor-pointer text-white sm:py-2 py-[0.6px] sm:px-3 rounded-md font-bold"
-            @click="this.$store.commit('logout')"
-            v-if="this.$store.state.loggedIn">
+          <a class="cursor-pointer text-white sm:py-2 py-[0.6px] sm:px-3 rounded-md font-bold"
+            @click="this.$store.commit('logout')" v-if="this.$store.state.loggedIn">
             Logout
           </a>
         </div>
