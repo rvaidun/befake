f8665a06869fba6dacf6d466a3234ae5dd247449
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2023-04-15 00:42:46 -0700

    removed upload profile picture

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -15,8 +15,8 @@ export default {
     PulseLoader,
     NavbarVue,
     UploadPost,
-    UploadProfilePicture
-},
+    UploadProfilePicture,
+  },
   async beforeMount() {
     event("view_posts", {
       event_category: "posts",
@@ -96,7 +96,7 @@ export default {
 };
 </script>
 <template>
-  <UploadProfilePicture :user="user" />
+  <!-- <UploadProfilePicture :user="user" /> -->
   <UploadPost v-if="!isfetch && !posts.posted" :user="user" />
   <div
     v-for="post in posts"
