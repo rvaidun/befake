935053f4ea99f994cbe3847fa6f37b4c64030b9d
xKRISTOFx <gral1322@gmail.com>
2023-04-17 01:06:37 +0200

    some explanantions

--- src/data/usercountry.js
+++ src/data/usercountry.js
@@ -1,3 +1,15 @@
+// This function returns the country of the user based on their IP address
+// It uses the ipregistry API to get the user's IP address and then searches for the country in the countries.js file
+// It returns the country information in an object
+// It uses the fetch API to make the request to ipregistry
+// It uses the async/await syntax to wait for the fetch API to return the response
+// It uses the findIndex method to search for the country in the countries array
+// It uses an arrow function to define the callback function for findIndex
+// It uses destructuring to get the country code from the response
+// It uses a template literal to print the country object
+// It uses the return statement to return the country object
+// The country object contains the country's name, its ISO code, and its phone code
+
 import countries from "./countries.js";
 const defcountry = async () => {
   let countrycode;
