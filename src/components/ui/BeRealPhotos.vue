f87d9501c7c4e245c4e4a557d54207b845009e31
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2023-06-06 22:51:26 -0700

    make images lazy loading

--- src/components/ui/BeRealPhotos.vue
+++ src/components/ui/BeRealPhotos.vue
@@ -12,12 +12,14 @@ export default {
 <template>
   <div class="relative top-0 left-0 justify-center">
     <img
+      loading="lazy"
       referrerpolicy="no-referrer"
       v-bind:src="reverse ? secondary : primary"
       class="relative top-0 left-0 rounded-md sm:w-[400px] w-[100%]"
       @click="hideSecondaryPhoto = !hideSecondaryPhoto"
       alt="postImage" />
     <img
+      loading="lazy"
       referrerpolicy="no-referrer"
       v-bind:src="reverse ? primary : secondary"
       class="absolute top-2 left-2 w-[35%] rounded-md border-2 border-black"
