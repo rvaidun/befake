578b278d6cdf85d5e81bcea22fce97144e9c2d33
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2023-08-23 23:50:29 -0700

    credits to rob--w

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// code written by Rob--W author of cors-anywhere
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || "0.0.0.0";
 // Listen on a specific port via the PORT environment variable
