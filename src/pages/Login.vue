d7aa27285eb2ecb74b8f08d8eab065153ba61a84
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2023-08-23 23:39:46 -0700

    remove firebase functions, bring login back into frontend, and add cors anywhere server

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -103,29 +103,104 @@ export default {
         this.loading = false;
         return;
       }
-      fetch(`${this.$store.state.loginUrl}`, {
-        method: "POST",
-        body: JSON.stringify({
-          phoneNumber: this.cc + this.phone,
-        }),
-      })
-        .then((res) => {
-          console.log(res.status);
-          return res.json();
-        })
+      fetch(
+        "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyClient?key=AIzaSyDwjfEeparokD7sXPVQli9NsTuhT6fJ6iA",
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            accept: "*/*",
+            "x-client-version": "iOS/FirebaseSDK/9.6.0/FirebaseCore-iOS",
+            "x-ios-bundle-identifier": "AlexisBarreyat.BeReal",
+            "accept-language": "en",
+            "user-agent":
+              "FirebaseAuth.iOS/9.6.0 AlexisBarreyat.BeReal/0.31.0 iPhone/14.7.1 hw/iPhone9_1",
+            "x-firebase-locale": "en",
+            "x-firebase-gmpid": "1:405768487586:ios:28c4df089ca92b89",
+          },
+          body: JSON.stringify({
+            appToken:
+              "54F80A258C35A916B38A3AD83CA5DDD48A44BFE2461F90831E0F97EBA4BB2EC7",
+          }),
+        }
+      )
+        .then((response) => response.json())
         .then((data) => {
-          if (data.error) {
-            throw Error(data.error.message);
-          }
-          this.sessionInfo = data.sessionInfo;
-          if (data.vonage) {
-            this.vonage = true;
-          }
-          this.loading = false;
-        })
-        .catch((e) => {
-          this.handleError(e);
-          this.loading = false;
+          console.log("this is the data receipt:", data.receipt);
+          fetch(
+            "https://www.googleapis.com/identitytoolkit/v3/relyingparty/sendVerificationCode?key=AIzaSyDwjfEeparokD7sXPVQli9NsTuhT6fJ6iA",
+            {
+              method: "POST",
+              headers: {
+                "content-type": "application/json",
+                accept: "*/*",
+                "x-client-version": "iOS/FirebaseSDK/9.6.0/FirebaseCore-iOS",
+                "x-ios-bundle-identifier": "AlexisBarreyat.BeReal",
+                "accept-language": "en",
+                "user-agent":
+                  "FirebaseAuth.iOS/9.6.0 AlexisBarreyat.BeReal/0.28.2 iPhone/14.7.1 hw/iPhone9_1",
+                "x-firebase-locale": "en",
+                "x-firebase-gmpid": "1:405768487586:ios:28c4df089ca92b89",
+              },
+              body: JSON.stringify({
+                iosReceipt: data.receipt,
+                phoneNumber: this.cc + this.phone,
+              }),
+            }
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.error) {
+                if (data.error.message === "APP_NOT_VERIFIED") {
+                  // handle app not verified
+                  fetch("https://auth.bereal.team/api/vonage/request-code", {
+                    method: "POST",
+                    headers: {
+                      accept: "application/json",
+                      "content-type": "application/json",
+                      "user-agent":
+                        "BeReal/7242 CFNetwork/1333.0.4 Darwin/21.5.0",
+                      "accept-language": "en-US,en;q=0.9",
+                    },
+                    body: JSON.stringify({
+                      phoneNumber: this.cc + this.phone,
+                      deviceId: "ntbgbuk8ly5gjvv3",
+                    }),
+                  })
+                    .then((response) => {
+                      if (!response.ok) {
+                        console.log("vonage error");
+                      }
+                      return response.json();
+                    })
+                    .then((data2) => {
+                      console.log("vonage data", data2);
+                      return {
+                        sessionInfo: data2.vonageRequestId,
+                        vonage: true,
+                      };
+                    });
+                } else {
+                  console.log(
+                    "sendVerificationCode responded with an error",
+                    data.error.message
+                  );
+                }
+              } else {
+                return data; // this is the sessionInfo
+              }
+            })
+            .then((data) => {
+              this.sessionInfo = data.sessionInfo;
+              if (data.vonage) {
+                this.vonage = true;
+              }
+              this.loading = false;
+            })
+            .catch((error) => {
+              this.handleError(error);
+              this.loading = false;
+            });
         });
     },
     verifyCode() {
